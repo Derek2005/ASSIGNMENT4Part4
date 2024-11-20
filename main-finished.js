a8b89af2586e23988edd402a1fbc5e13bba74666
Uvie Udjo <derek.ud123@gmail.com>
2024-11-20 14:30:29 -0400

    Update main-finished.js

--- main-finished.js
+++ main-finished.js
@@ -1,18 +1,12 @@
-// set up canvas
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// function to generate random RGB color value
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
@@ -46,19 +40,15 @@ class Ball extends Shape {
     if (this.x + this.size >= width) {
       this.velX = -Math.abs(this.velX);
     }
-
     if (this.x - this.size <= 0) {
       this.velX = Math.abs(this.velX);
     }
-
     if (this.y + this.size >= height) {
       this.velY = -Math.abs(this.velY);
     }
-
     if (this.y - this.size <= 0) {
       this.velY = Math.abs(this.velY);
     }
-
     this.x += this.velX;
     this.y += this.velY;
   }
@@ -69,7 +59,6 @@ class Ball extends Shape {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
@@ -83,7 +72,6 @@ class EvilCircle extends Shape {
     super(x, y, 20, 20);
     this.color = 'white';
     this.size = 10;
-
     window.addEventListener('keydown', (e) => {
       switch (e.key) {
         case 'a':
@@ -114,15 +102,12 @@ class EvilCircle extends Shape {
     if (this.x + this.size >= width) {
       this.x = width - this.size;
     }
-
     if (this.x - this.size <= 0) {
       this.x = this.size;
     }
-
     if (this.y + this.size >= height) {
       this.y = height - this.size;
     }
-
     if (this.y - this.size <= 0) {
       this.y = this.size;
     }
@@ -134,7 +119,6 @@ class EvilCircle extends Shape {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-
         if (distance < this.size + ball.size) {
           ball.exists = false;
         }
@@ -148,8 +132,6 @@ const balls = [];
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
@@ -157,7 +139,6 @@ while (balls.length < 25) {
     randomRGB(),
     size
   );
-
   balls.push(ball);
 }
 
